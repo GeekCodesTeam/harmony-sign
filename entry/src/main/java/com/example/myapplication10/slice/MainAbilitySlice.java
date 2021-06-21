2803729c274988b6b89fc7673d53637b58e6e068
liangxiao6 <liangxiao6@live.com>
2021-06-21 15:51:58 +0800

    merger

---
+++ entry/src/main/java/com/example/myapplication10/slice/MainAbilitySlice.java
@@ -0,0 +1,24 @@
+package com.example.myapplication10.slice;
+
+import com.example.myapplication10.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+//        https://mp.weixin.qq.com/s/PvMFKCh_9azCsDDd6l2eOQ
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
